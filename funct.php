bf8542c2e35f1132ea59167bf2b8f6a9d93384d9
aluvium <aluvium@protonmail.com>
2022-07-29 06:58:57 +0000

    functions

---
+++ funct.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html><head><title>PHP Store</title></head>
+  <body>
+    <?php
+ //   $nr1=800 ;
+ //   $nr2=0.0825 ;
+
+      function calc($nr1,$nr2) {
+          $sum= $nr1 * $nr2 ;
+		  $tot= round($tot+$sum,2) ;
+		  return $tot ;  
+      }
+      echo calc(755,0.0725) ;
+    ?>
+  </body>
+</html>
+
