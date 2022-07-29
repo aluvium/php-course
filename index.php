64fe3f7aa6bc62f31551387c575dfb9fcee2285d
aluvium <aluvium@protonmail.com>
2022-07-29 07:21:06 +0000

    shop

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>PHP Store</title>
-  </head>
+<html><head><title>PHP Store</title></head>
   <body>
     <?php
-      $name="LAMP - SERVER";
-      $var="<pre>"  .exec("openssl rand -hex 40"). "</pre>" ;
-      $date="<pre>" .exec("date"). "</pre>" ;
-      echo "<h1>Hello in ".$name." muahaaahhaha!</h1>";
-      echo "<h2>My favorite number: ".$var."</h2>";
-      echo "<h2>Today we have ".$date." which is lucky day</h2/>";
+        $name = "PHP Store";
+        $credit = 1025; 
+	echo "<h1>Welcome to ".$name."!</h1>";
+	echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+        $products['Computer']=750;
+	$products['Car']=15000;
+	$products['iPhone']=1000;
+	$products['Toaster']=75;
+        $taxRate=0.0825;
+
+        function tax_calc( $amount,$tax ){
+	    $addedTax = $amount*$tax;
+	    $amountWithTax = round( $amount+$addedTax,2 ) ;
+	    return $amountWithTax;
+    	}
+
+        foreach( $products as $key => $value ){
+            $costWithTax = tax_calc( $value,$taxRate );
+            echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
+	    }
+	
+        echo "<h2>Items you can't afford</h2>";
+        foreach( $products as $key => $value ){
+	    $costWithTax = tax_calc( $value,$taxRate ) ;
+	    if( $costWithTax >= $credit ){
+                echo "<p>".$key."</p>"; 
+	    }
+	}
     ?>
   </body>
 </html>
